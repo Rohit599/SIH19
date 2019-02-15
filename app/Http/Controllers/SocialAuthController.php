7af995b1a9d7b4c260272a69d2a97787358708eb
Rohit Jakhmola <rohit@localhost.localdomain>
2019-02-15 23:07:51 +0530

    Fix Standard issue

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -7,13 +7,15 @@ use Socialite;
 
 class SocialAuthController extends Controller
 {
-    public function redirect($service) {
-        return Socialite::driver( $service )->redirect ();
+    public function redirect($service)
+    {
+        return Socialite::driver($service)->redirect();
     }
 
-    public function callback($service) {
-        $user = Socialite::with( $service )->user ();
-        // dd($user->getName());
-        return view( 'home' )->with( compact('user') )->withService( $service );
+    public function callback($service)
+    {
+        $user = Socialite::with($service)->user();
+        // dd($user);
+        return view('home')->with(compact('user'))->withService($service);
     }
 }
