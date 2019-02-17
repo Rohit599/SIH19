5aea22dbaa76734b5abff96a24780134ff504dcb
Anant Jain <anantjain60@gmail.com>
2019-02-17 13:07:45 +0530

    Modal Relationship & Pollution Seeder

--- app/Pollution.php
+++ app/Pollution.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pollution extends Model
 {
-    //
+    /**
+     * Get the issues of a pollution type.
+     */
+    public function issues()
+    {
+        return $this->hasMany('App\Issue');
+    }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    /**
+     * Get the blog posts with tag.
+     */
+    public function blogs()
+    {
+        return $this->belongsToMany('App\Blog', 'blog_tag');
+    }
 }
