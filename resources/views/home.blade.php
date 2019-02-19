1e04271cebbe427edb5bf20bbaa419bf75ce25c1
Anant Jain <anantjain60@gmail.com>
2019-02-19 23:06:22 +0530

    Login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,7 @@
 @extends('layouts.default')
 @section('content')
-@if($service == 'facebook')
 <div class="title m-b-md">
-    Welcome {{ $user->getName() }} ! <br> .
     <a href="{{url('/issue/create')}}">Add Issue</a>
+    <a href="{{url('logout')}}">Logout</a>
 </div>
-@endif
-
-@if($service == 'twitter')
-<div class="title m-b-md">
-    Welcome {{ $user->getName() }} ! <br> 
-    <a href="{{url('/issue/create')}}">Add Issue</a>
-    {{-- Your username is : {{ $details->nickname }}<br>
-    Total Tweets : {{ $details->user['statuses_count']}}<br>
-    Followers : {{ $details->user['followers_count']}}<br>
-    Following : {{ $details->user['friends_count']}} --}}
-</div>
-@endif
 @stop
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,31 +1,4 @@
-        <footer class="footer text-center">
-            &copy; All Rights Reserved by <a href="http://hackncs.com" target="_blank">Tesla 2.O</a>.
-        </footer>
-        <!-- ============================================================== -->
-        <!-- End footer -->
-        <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- End Page wrapper  -->
-    <!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- End Wrapper -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- All Jquery -->
-<!-- ============================================================== -->
-{{-- <script src="{{ url('assets/libs/jquery/dist/jquery.min.js') }}"></script> --}}
-<!-- Bootstrap tether Core JavaScript -->
-{{-- <script src="{{ url('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script> --}}
-{{-- <script src="{{ url('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script> --}}
-{{-- <script src="{{ url('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script> --}}
-{{-- <script src="{{ url('assets/extra-libs/sparkline/sparkline.js') }}"></script> --}}
-<!--Wave Effects -->
-{{-- <script src="{{ url('dist/js/waves.js') }}"></script> --}}
-<!--Menu sidebar -->
-{{-- <script src="{{ url('dist/js/sidebarmenu.js') }}"></script> --}}
-<!--Custom JavaScript -->
-{{-- <script src="{{ url('dist/js/custom.min.js') }}"></script> --}}
-<!--This page JavaScript -->
-<!-- <script src="../../dist/js/pages/dashboards/dashboard1.js"></script> -->
-
+<footer class="footer text-center">
+    &copy; All Rights Reserved by <a href="{{ url('/') }}" target="_blank">{{ config('app.name') }}</a>.
+</footer>
 @yield('script')
